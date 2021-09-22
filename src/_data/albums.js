c31b13993bb7851804c13797f57db0d8c5f7d491
James Muspratt <james@jamesmuspratt.com>
2021-09-22 11:43:22 -0400

    Refine 2-up portrait image layout

--- src/_data/albums.js
+++ src/_data/albums.js
@@ -11,23 +11,45 @@ const scanLibrary = (pathString) => {
 
         const albumContentsPath = path.resolve(process.cwd(), `${pathString}/${slug}` );
         const albumContents = fs.readdirSync(albumContentsPath, 'utf-8');
-        const name = slug.substring(11).replace('-', ' ');
+        const name = slug.substring(11).replace(/-/g, ' ');
         
         const files = albumContents.map(name=>{
 
             const extension = name.split('.')[1];
+            
+            let fileType = null;
+            switch(extension) {
+                case 'txt' :
+                    fileType = 'text';
+                    break;
+                case 'md' :
+                    fileType = 'text';
+                    break;
+                case 'mov' :
+                    fileType = 'video';
+                    break;
+                case 'mp4' :
+                    fileType = 'video';
+                    break;
+                case 'jpg' :
+                    fileType = 'image';
+                    break;
+                default: 
+                    fileType = null;
+            }
             let textHeading = null;
             let textContents = null;
 
             // text files
             if (['txt', 'md'].includes(extension)) {
-                textHeading = name.substring(20).replace('-', ' ').replace(`.${extension}`, '');
+                textHeading = name.substring(20).replace(/-/g, ' ').replace(`.${extension}`, '');
                 textContents = fs.readFileSync(`${albumContentsPath}/${name}`).toString();
             }
 
             return {
                 name,
                 extension,
+                fileType,
                 textHeading,
                 textContents
             };
